06ac89536ceb98e185c4c0b384820f7457b5e084
Myeonghyeon Kim <myeonghyeon@scatterlab.co.kr>
2023-08-20 02:04:54 +0900

    feat: add restaurant info to chat room

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,10 +1,10 @@
 import { css } from '@emotion/react';
 import { memo } from 'react';
-import { IconButton, useTheme } from '@mui/material';
+import { Button, IconButton, useTheme } from '@mui/material';
 import MyUtterance from '../common/components/MyUtterance';
 import OpponentUtterance from '../common/components/OpponentUtterance';
 import { ReactComponent as ChevronLeftIcon } from '../assets/icons/chevron-left.svg';
-
+import sampleFood1Image from '../assets/sample-food-1.png';
 const ChatListPage = () => {
   const theme = useTheme();
 
@@ -46,6 +46,77 @@ const ChatListPage = () => {
         >
           Wade
         </h1>
+      </div>
+
+      <div
+        css={css`
+          padding: 0 20px;
+          display: flex;
+          position: relative;
+          justify-content: center;
+          align-items: center;
+          height: 50px;
+          border-bottom: 1px solid ${theme.palette.gray[20]};
+        `}
+      >
+        <img
+          src={sampleFood1Image}
+          alt="sample-food-1"
+          css={css`
+            width: 36px;
+            height: 36px;
+            border-radius: 4px;
+            border: 1px solid ${theme.palette.gray[20]};
+            margin-right: 12px;
+          `}
+        />
+        <h2
+          css={css`
+            color: ${theme.palette.gray[100]};
+            font-size: 14px;
+            font-weight: 510;
+            line-height: 140%;
+            letter-spacing: -0.056px;
+          `}
+        >
+          Lacoon Pizza
+        </h2>
+        <div
+          css={css`
+            width: 1px;
+            height: 10px;
+            background-color: ${theme.palette.gray[40]};
+            margin: 0 6px;
+          `}
+        />
+        <p
+          css={css`
+            color: ${theme.palette.gray[40]};
+            font-size: 12px;
+            font-weight: 274;
+            line-height: 140%;
+            letter-spacing: -0.048px;
+          `}
+        >
+          279m
+        </p>
+        <div
+          css={css`
+            flex: 1;
+          `}
+        />
+        <Button
+          color="primary"
+          variant="outlined"
+          css={css`
+            border-radius: 6px;
+            text-transform: none;
+            font-size: 14px;
+            font-weight: 600;
+          `}
+        >
+          Invite
+        </Button>
       </div>
       <div
         css={css`
