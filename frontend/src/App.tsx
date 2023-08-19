022fc7c6bb3a7096166dd589f298c63fd6629c86
Handong Kim <aggsae@gmail.com>
2023-08-19 10:24:52 +0900

    feat: google-on-tap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,24 @@ import HelloWorld from './HelloWorld';
 import { Global } from '@emotion/react';
 import globalStyles from './styles/globalStyles';
 import Swipe from './Swipe';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './common/auth';
+
+const queryClient = new QueryClient();
 
 const App: FC = () => (
   <StrictMode>
     <Global styles={globalStyles} />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HelloWorld />} />
-        <Route path="/swipe" element={<Swipe />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HelloWorld />} />
+            <Route path="/swipe" element={<Swipe />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
 
---
+++ frontend/src/common/auth.tsx
@@ -0,0 +1,188 @@
+/* eslint-disable no-console */
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import axios, { AxiosInstance } from 'axios';
+import { googleLoginApi, refreshTokenApi } from './apis';
+import useScript, { ScriptStatus } from './hooks/useScript';
+import { LocalStorageKey } from './types';
+import { useMutation } from '@tanstack/react-query';
+
+axios.defaults.paramsSerializer = {
+  indexes: null,
+};
+
+const AxiosContext = createContext<AxiosInstance>(
+  axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  })
+);
+
+interface AuthContextValue {
+  isAuthenticated: boolean;
+  setGoogleIdToken: React.Dispatch<React.SetStateAction<string | null>>;
+  logout: () => void;
+}
+
+declare global {
+  const google: typeof import('google-one-tap');
+}
+
+const AuthContext = createContext<AuthContextValue>({
+  isAuthenticated: !!localStorage.getItem('accessToken'),
+  setGoogleIdToken: () => {},
+  logout: () => {},
+});
+
+const withLock = async (lockName: string, callback: () => Promise<void>) => {
+  await navigator.locks.request(
+    lockName,
+    { ifAvailable: true },
+    async (lock) => {
+      if (lock) {
+        await callback();
+      } else {
+        await navigator.locks.request(lockName, () => {}); // just wait
+      }
+    }
+  );
+};
+
+interface AxiosProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AxiosProviderProps) => {
+  const [willLogout, setWillLogout] = useState<boolean>(false);
+  const [googleIdToken, setGoogleIdToken] = useState<string | null>(null);
+  const googleScriptStatus = useScript(
+    'https://accounts.google.com/gsi/client',
+    willLogout
+  );
+  const [accessToken, setAccessToken] = useState<string | null>(
+    localStorage.getItem(LocalStorageKey.AccessToken)
+  );
+
+  const axiosRef = useRef(
+    axios.create({
+      baseURL: import.meta.env.VITE_BACKEND_API_URL,
+    })
+  );
+
+  const { mutateAsync: refreshTokenRequest } = useMutation({
+    mutationKey: ['refreshToken'],
+    mutationFn: (token: string) => refreshTokenApi(token),
+  });
+  const { mutateAsync: googleLoginrequest } = useMutation(googleLoginApi);
+
+  const logout = useCallback(() => {
+    setWillLogout(true);
+  }, []);
+
+  useEffect(() => {
+    if (!willLogout || googleScriptStatus !== ScriptStatus.Ready) return;
+    google.accounts.id.disableAutoSelect();
+
+    setAccessToken(null);
+    setGoogleIdToken(null);
+    localStorage.removeItem(LocalStorageKey.AccessToken);
+    localStorage.removeItem(LocalStorageKey.RefreshToken);
+  }, [willLogout, googleScriptStatus]);
+
+  const refreshAccessToken = useCallback(async () => {
+    const refreshToken = localStorage.getItem(LocalStorageKey.RefreshToken);
+    if (!refreshToken) return null;
+    await withLock('refreshToken', async () => {
+      try {
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          await refreshTokenRequest(refreshToken);
+
+        setAccessToken(newAccessToken);
+
+        localStorage.setItem(LocalStorageKey.AccessToken, newAccessToken);
+        localStorage.setItem(LocalStorageKey.RefreshToken, newRefreshToken);
+      } catch {
+        logout();
+      }
+    });
+    return localStorage.getItem(LocalStorageKey.AccessToken);
+  }, [refreshTokenRequest, logout]);
+
+  useLayoutEffect(() => {
+    if (!accessToken) return undefined;
+
+    const axiosRefCurrent = axiosRef.current;
+
+    const reqInterceptorId = axiosRefCurrent.interceptors.request.use(
+      async (config) => {
+        try {
+          config.headers.set('Authorization', `Bearer ${accessToken}`);
+        } catch (error) {
+          console.error(error);
+        }
+        return config;
+      }
+    );
+
+    const resInterceptorId = axiosRefCurrent.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401) {
+          const newAccessToken = await refreshAccessToken();
+          if (newAccessToken) {
+            originalRequest.headers.set(
+              'Authorization',
+              `Bearer ${newAccessToken}`
+            );
+            return axios(originalRequest);
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosRefCurrent.interceptors.request.eject(reqInterceptorId);
+      axiosRefCurrent.interceptors.response.eject(resInterceptorId);
+    };
+  }, [accessToken, refreshAccessToken]);
+
+  useEffect(() => {
+    if (accessToken || !googleIdToken) return;
+
+    (async () => {
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+        await googleLoginrequest(googleIdToken);
+
+      setAccessToken(newAccessToken);
+
+      localStorage.setItem(LocalStorageKey.AccessToken, newAccessToken);
+      localStorage.setItem(LocalStorageKey.RefreshToken, newRefreshToken);
+    })();
+  }, [googleIdToken, accessToken, googleLoginrequest]);
+
+  const value = useMemo(
+    () => ({ isAuthenticated: !!accessToken, setGoogleIdToken, logout }),
+    [accessToken, setGoogleIdToken, logout]
+  );
+
+  return (
+    <AxiosContext.Provider value={axiosRef.current}>
+      <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    </AxiosContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
+export const useAxios = () => useContext(AxiosContext);
