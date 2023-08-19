c9829d6094320805551048613b295eb0807c4507
Handong Kim <aggsae@gmail.com>
2023-08-19 10:31:35 +0900

    feat: google login button 추가

--- frontend/src/HelloWorld.tsx
+++ frontend/src/HelloWorld.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
+import GoogleLoginButton from './common/components/GoogleLoginButton';
 
 const HelloWorld: FC = () => (
   <div>
     <h1 css={{ fontSize: '48px', fontWeight: 590 }}>sia</h1>
-    Hello world!
+    <GoogleLoginButton />
   </div>
 );
 
---
+++ frontend/src/common/components/GoogleLoginButton.tsx
@@ -0,0 +1,48 @@
+import { FC, memo, useEffect, useRef } from 'react';
+import { useAuth } from '../auth';
+import useScript, { ScriptStatus } from '../hooks/useScript';
+import { css } from '@emotion/react';
+
+const containerStyle = css`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const GoogleLoginButton: FC = () => {
+  const { setGoogleIdToken } = useAuth();
+  const googleButton = useRef<HTMLDivElement>(null);
+  const googleScriptStatus = useScript(
+    'https://accounts.google.com/gsi/client'
+  );
+
+  useEffect(() => {
+    if (googleScriptStatus !== ScriptStatus.Ready || !googleButton.current)
+      return;
+
+    google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID ?? '',
+      auto_select: true,
+      callback: ({ credential }) => {
+        setGoogleIdToken(credential);
+      },
+    });
+
+    google.accounts.id.renderButton(googleButton.current, {
+      theme: 'outline',
+      size: 'large',
+    });
+  }, [googleScriptStatus, setGoogleIdToken]);
+
+  return (
+    <div css={containerStyle}>
+      <div ref={googleButton} css={{ marginTop: '10px' }} />
+    </div>
+  );
+};
+
+export default memo(GoogleLoginButton);
