3ae10bb38380ff3d3350753e34b0d190fea06e95
Myeonghyeon Kim <myeonghyeon@scatterlab.co.kr>
2023-08-19 09:35:58 +0900

    fix: emotion 활성화

--- frontend/src/HelloWorld.tsx
+++ frontend/src/HelloWorld.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const HelloWorld: FC = () => (
   <div>
-    <h1 style={{ fontSize: '48px', fontWeight: 590 }}>sia</h1>
+    <h1 css={{ fontSize: '48px', fontWeight: 590 }}>sia</h1>
     Hello world!
   </div>
 );
