95806053d03917429cd57ebe40544018b7f858cf
Myeonghyeon Kim <myeonghyeon@scatterlab.co.kr>
2023-08-19 19:57:28 +0900

    fix: layout (#18)

--- frontend/src/common/components/Navbar.tsx
+++ frontend/src/common/components/Navbar.tsx
@@ -1,21 +1,25 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import logo from '../../assets/logo.png';
 import { memo } from 'react';
 import { css } from '@emotion/react';
-import { IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import { ReactComponent as NotificationIcon } from '../../assets/icons/notification.svg';
 import { ReactComponent as UserFavoriteIcon } from '../../assets/icons/user-favorite.svg';
+import { ReactComponent as PlusIcon } from '../../assets/icons/plus.svg';
 
 const Navbar = () => {
   return (
     <AppBar
-      position="static"
+      position="absolute"
       color="transparent"
       elevation={0}
+      variant="outlined"
       css={css`
+        top: auto;
+        bottom: 0;
         & .MuiToolbar-root {
-          min-height: 50px;
+          min-height: 80px;
+          height: 80px;
         }
       `}
     >
@@ -23,16 +27,32 @@ const Navbar = () => {
         <div
           css={css`
             flex-grow: 1;
+            display: flex;
+            gap: 10px;
           `}
         >
-          <img src={logo} alt="logo" />
+          <IconButton color="inherit" size="large">
+            <NotificationIcon />
+          </IconButton>
+          <IconButton color="inherit" size="large">
+            <UserFavoriteIcon />
+          </IconButton>
         </div>
-        <IconButton color="inherit">
-          <NotificationIcon />
-        </IconButton>
-        <IconButton color="inherit">
-          <UserFavoriteIcon />
-        </IconButton>
+        <Button
+          color="primary"
+          variant="contained"
+          css={css`
+            width: 56px;
+            min-width: 56px;
+            height: 56px;
+            border-radius: 16px;
+            box-shadow:
+              0px 1px 3px 0px rgba(0, 0, 0, 0.3),
+              0px 4px 8px 3px rgba(0, 0, 0, 0.15);
+          `}
+        >
+          <PlusIcon />
+        </Button>
       </Toolbar>
     </AppBar>
   );
