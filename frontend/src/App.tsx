2f216f35a6144c41d73174dbed95778382451489
Myeonghyeon Kim <myeonghyeon@scatterlab.co.kr>
2023-08-19 10:58:16 +0900

    feat: add max-width for the view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,29 @@
 import { FC, StrictMode } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HelloWorld from './HelloWorld';
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import globalStyles from './styles/globalStyles';
 import Swipe from './Swipe';
 
 const App: FC = () => (
   <StrictMode>
     <Global styles={globalStyles} />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HelloWorld />} />
-        <Route path="/swipe" element={<Swipe />} />
-      </Routes>
-    </BrowserRouter>
+    <div
+      css={css`
+        margin: 0 auto;
+        background-color: white;
+        max-width: 430px;
+        position: relative;
+        overflow: hidden;
+      `}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloWorld />} />
+          <Route path="/swipe" element={<Swipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   </StrictMode>
 );
 
--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -11,7 +11,7 @@ const globalStyles = css`
     font-family: monospace;
   }
   html {
-    overflow: hidden;
+    background-color: black;
   }
 `;
 
